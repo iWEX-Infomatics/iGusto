95ab5357da64a28c502001b0a17e816ed084cdd0
imadhubala27 <imadhubala27@gmail.com>
2025-11-08 10:22:07 +0530

    changes

--- igusto/igusto/doctype/room_orders/room_orders.js
+++ igusto/igusto/doctype/room_orders/room_orders.js
@@ -1,8 +1,16 @@
-// Copyright (c) 2025, madhu and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Extras Order', {
+    extra_quantity: function(frm, cdt, cdn) {
+        calculate_extra_amount(frm, cdt, cdn);
+    },
+    extra_price: function(frm, cdt, cdn) {
+        calculate_extra_amount(frm, cdt, cdn);
+    }
+});
 
-// frappe.ui.form.on("Room Orders", {
-// 	refresh(frm) {
-
-// 	},
-// });
+function calculate_extra_amount(frm, cdt, cdn) {
+    let row = frappe.get_doc(cdt, cdn);
+    if (row.extra_quantity && row.extra_price) {
+        row.extra_amount = flt(row.extra_quantity) * flt(row.extra_price);
+        frm.refresh_field('room_order_extras');
+    }
+}
